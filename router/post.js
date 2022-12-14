3a775981fcb78f87f146d30f7eabb8abc4186588
Marlon GARCIA <clasesucatmarlon@gmail.com>
2022-12-14 10:01:28 -0500

    feat: :art: structure api of post created
    Structure api of post created

---
+++ router/post.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const postController = require('../controllers/post');
+const md_auth = require('../middlewares/authenticated');
+const multipart = require('connect-multiparty');
+
+const md_upload = multipart({ uploadDir: './uploads/posts' });
+
+const api = express.Router();
+
+// RUTAS
+api.post('/post', [md_auth.asureAuth], postController.createPost);
+
+module.exports = api;
